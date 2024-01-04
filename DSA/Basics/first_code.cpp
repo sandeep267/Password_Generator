bc03888e95ba82e521275f6186fff33fbd35475e
sandeep267 <111731650+sandeep267@users.noreply.github.com>
2024-01-04 14:48:09 +0530

    Add files via upload

---
+++ DSA/Basics/first_code.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,sum;
+    cout<<"enter 1st number : ";
+    cin>>a;
+
+    cout<<"enter 2nd number : ";
+    cin>>b;
+    sum=a+b;
+
+    cout<<"Sum is : "<<sum<<endl;
+    return 0;
+} // namespace std;
+
